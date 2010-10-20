93503ba95340994ea545351be332eeefb1ee2322
Takuro IIZUKA <iitaku@gmail.com>
2010-10-21 00:54:37 +0900

    add puxval

--- lib.c
+++ lib.c
@@ -95,25 +95,23 @@ int strncmp (const char* s1, const char* s2, int len)
 
 int putxval(unsigned long value, int column)
 {
-    char buf[9] = {0};
-    unsigned int i=sizeof(unsigned long)*8;
-    unsigned int j;
-    unsigned int shiftWidth = sizeof(unsigned long)*8-4;
-    unsigned long mask = 0xFul<<shiftWidth;
-    
-    buf[0] = '\0';
-    
-    for (; i>0; i-=4, value<<=4) {
-        if (mask & value) {
-            break;
-        }
+    char buf[9];
+    char *p;
+
+    p = buf + sizeof(buf) - 1;
+    *(p--) = '\0';
+
+    if (!value && !column) {
+        column++;
     }
-    
-    for (j=0; i>0; i-=4, value<<=4, j++) {
-        buf[j] = "0123456789abcdef"[(mask & value)>>shiftWidth];
+
+    while (value || column) {
+        *(p--) = "0123456789abcdef"[value & 0xf];
+        value >>=4;
+        if (column) column--;
     }
-    
-    puts(buf);
+
+    puts(p+1);
 
     return 0;
 }
--- test.c
+++ test.c
@@ -16,7 +16,11 @@ int putxval(unsigned long value, int column)
         }
     }
     
-    for (j=0; i>0; i-=4, value<<=4, j++) {
+    for (j=0; j<column-(i>>2); j++) {
+        buf[j] = '0';
+    }
+
+    for (; i>0; i-=4, value<<=4, j++) {
         buf[j] = "0123456789abcdef"[(mask & value)>>shiftWidth];
     }
     
@@ -25,7 +29,28 @@ int putxval(unsigned long value, int column)
     return 0;
 }
 
+int putxval2(unsigned long value, int column)
+{
+    char buf[9];
+    char *p;
+
+    p = buf + sizeof(buf) - 1;
+    *(p--) = '\0';
+
+    if (!value && !column)
+        column++;
+
+    while (value || column) {
+        *(p--) = "0123456789abcdef"[value & 0xf];
+        value >>=4;
+        if (column) column--;
+    }
+
+    puts(p+1);
+}
+
 int main ()
 {
     putxval(16, 0);
+    putxval2(16, 3);
 }
